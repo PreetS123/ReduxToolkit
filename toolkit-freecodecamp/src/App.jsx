5408eb2b0e7eed1d7edcb0f70a00190890dd5f00
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-13 19:15:41 +0530

    basic navbar created

--- toolkit-freecodecamp/src/App.jsx
+++ toolkit-freecodecamp/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-       
-      </div>
-      
-    </>
+    <main>
+       <Navbar/>      
+    </main>
   )
 }
 

d4a61aed315dfcf3e0d4eb5c2951efc1deb1a8cb
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-14 13:02:21 +0530

    data comming using api by axios

--- toolkit-freecodecamp/src/App.jsx
+++ toolkit-freecodecamp/src/App.jsx
@@ -1,32 +1,40 @@
-import { useEffect} from 'react';
-import {useDispatch,useSelector} from "react-redux";
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 import Navbar from "./components/Navbar";
-import CartContainer from './components/CartContainer';
-import { calculateTotals } from './features/cart/cartSlice';
-import Modal from './components/Modal';
+import CartContainer from "./components/CartContainer";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
-  const {cartItems}= useSelector((store)=>store.cart);
-  const dispatch= useDispatch()
-  const {isModalOpen}= useSelector((store)=>store.model)
-  
-  useEffect(()=>{
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  const { isModalOpen } = useSelector((store) => store.model);
+
+  useEffect(() => {
     dispatch(calculateTotals());
-  },[cartItems])
-  
+  }, [cartItems]);
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
 
   return (
     <>
-    <main>
-       <Navbar/>  
-       <CartContainer/>    
-    </main>
-    {
-    isModalOpen? <Modal/>:<></>
-    }
+      <main>
+        <Navbar />
+        <CartContainer />
+      </main>
+      {isModalOpen ? <Modal /> : <></>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- toolkit-freecodecamp/src/features/cart/cartSlice.js
+++ toolkit-freecodecamp/src/features/cart/cartSlice.js
@@ -1,12 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// import cartItems from "../../cartItems";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
-  cartItems: cartItems,
-  amount: cartItems.length,
+  cartItems: [],
+  amount: 0,
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (_, thunkAPI) => {
+    try {
+      // console.log(thunkAPI.getState());
+      const resp = await axios.get(url);
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue("error", err.message);
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -14,36 +30,50 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
-      state.amount=0;
+      state.amount = 0;
     },
-    removeItem:(state,action)=>{
-        const itemId= action.payload;
-        state.cartItems= state.cartItems.filter((item)=>item.id !==itemId);
-        state.amount= state.amount-1;
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      state.amount = state.amount - 1;
     },
-    increase:(state,{payload})=>{
-        const cartItem=state.cartItems.find((item)=>item.id===payload.id)
-        cartItem.amount= cartItem.amount+1;
-        state.amount=state.amount+1;
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+      state.amount = state.amount + 1;
     },
-    decrease:(state,{payload})=>{
-        const cartItem=state.cartItems.find((item)=>item.id===payload.id)
-        cartItem.amount= cartItem.amount-1;
-        state.amount= state.amount-1;
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+      state.amount = state.amount - 1;
     },
-    calculateTotals:(state)=>{
-       let amount=0;
-       let total=0;
-       state.cartItems.forEach((item)=>{
-        amount +=item.amount;
-        total+=item.amount * item.price;
-       });
-       state.amount= amount;
-       state.total=total;
-    }
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      // console.log(action.payload);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart,removeItem , increase, decrease, calculateTotals} = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
 export default cartSlice.reducer;
